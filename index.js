8f2f170e540543586d0fb7985ca50530518eadd2
emrullahtastan <emrullah69@gmail.com>
2022-05-12 16:31:15 +0300

    add express, add notes endpoint

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const http = require('http')
-http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(9090, ()=>{
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("It's running.")
+})
+
+app.get('/notes', (req, res) => {
+
+    let list = [{id: 1, name: 'Vardım ki Yurdumda'}, {id: 2, name: 'Ben Feleği Gördüm'}, {id: 3, name: 'Benim Ağam'}]
+
+    res.send(list)
+})
+
+app.listen(9090, () => {
     console.log('Server running at http://localhost:9090');
 })
